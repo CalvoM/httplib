0fb6e7d3b6d5a717fed7c65528ad47f575d0f1be
CalvoM <mwadimemakokha@ieee.org>
2020-03-27 00:43:00 +0300

    Remove debug statements.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ int main(){
         Println("Nothing bound!")
     }
     while(server.listen_for_clients(MAX_CONNECTIONS)!=0);
-    while(server.accept_client_connection()==-1){
-        Println("...")
-    };
-    Println("===")
+    while(server.accept_client_connection()==-1);
     int n = server.read_html_page();
     server.prepare_response(n);
     Println("Sent %d bytes",server.send_to_client());
