2d96d512c141a33a0ea1a0500c9817cdeb522c0c
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-23 06:15:28 +0300

    POC for zlib

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # httplib
 
 Library for HTTP operations in C++.
+
+## Additional Libraries
+- [Zlib](https://zlib.net) - After download, just run the tests and then install the libraries.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include "Client/HTTPClient.h"
+#include <zlib.h>
 
+void compressBody(string body){
+    const char *pData = body.c_str();
+    size_t dataSize = body.size();
+    cout<<"Initial body size: "<<dataSize<<std::endl;
+    size_t compressedDataSize = dataSize;
+    unsigned char* compressedData = new unsigned char[compressedDataSize];
+    int result = compress2(compressedData,&compressedDataSize,(const unsigned char*)pData,dataSize,9);
+    if(result == Z_OK){
+        cout<<"Compressed size: "<<compressedDataSize<<std::endl;
+
+    }else{
+        cout<<"Issue during compression";
+    }
+    delete []compressedData;
+}
 
 int main(int argc, char **argv){
     HTTPClient client("eu.httpbin.org");
     auto resp = client.Get("/");
-    cout<<resp.getBody();
+    string body = resp.getBody();
+    compressBody(body);
 }
