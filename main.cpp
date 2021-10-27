54c5d37d1ba63a3258f027df3476e7be220212b0
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-27 15:20:36 +0300

    User can add params successfully to the url

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char **argv){
     HTTPClient client("neverssl.com");
-    auto resp = client.Get("/");
+    ParamsData p;
+    p["marco"] = "polo";
+    p["got"] = "good";
+    auto resp = client.Get("/",&p);
     string body = resp.getBody();
     cout<<body;
 }
